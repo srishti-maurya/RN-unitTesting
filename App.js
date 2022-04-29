6b0aa94b9bdafd8b7550a33bbeb66b45e228c4c8
srishti-maurya <39724354+srishti-maurya@users.noreply.github.com>
2022-04-29 18:01:27 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>This is a demo for unit testing</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
