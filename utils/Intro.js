28934e66e3bd8b48bb987d426d99f1b983257a47
srishti-maurya <39724354+srishti-maurya@users.noreply.github.com>
2022-05-06 12:02:39 +0530

    changed class component

--- utils/Intro.js
+++ utils/Intro.js
@@ -1,17 +1,15 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-class Intro extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>
-          This is a React Native snapshot test.
-        </Text>
-      </View>
-    );
-  }
+function Intro() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>
+        This is a React Native snapshot test.
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
